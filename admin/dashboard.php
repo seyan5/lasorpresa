7b4ed1a010f9b509e7650862434939a84a6d8087
jwaynee <115685795+jwayne5@users.noreply.github.com>
2024-11-11 19:07:04 +0800

    1

--- admin/dashboard.php
+++ admin/dashboard.php
@@ -43,7 +43,7 @@ if ($result->num_rows > 0) {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>La Sorpresa Admin</title>
     <!-- ======= Styles ====== -->
-    <link rel="stylesheet" href="../assets/css/style.css">
+    <link rel="stylesheet" href="../css/style.css">
 </head>
 
 <body>
