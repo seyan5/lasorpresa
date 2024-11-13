22bc4b39c1f98af82e50042f20deb168459143b6
jay <jaycantemprate31@gmail.com>
2024-11-13 12:45:45 +0800

    "Updated CSS, PHP, and JavaScript files to add filtering and sorting functionality to product list, and made minor changes to HTML structure and content."git

--- index.php
+++ index.php
@@ -185,12 +185,12 @@ $conn->close();
             <div class="overlay">
                 <div class="overlay-panel overlay-left">
                     <h1>Welcome Back!</h1>
-                    <p>To keep connected with us please login with your personal info</p>
+                    <p>To keep connected with us please login with your personal info.</p>
                     <button id="signIn">Sign In</button>
                 </div>
                 <div class="overlay-panel overlay-right">
                     <h1>Hello, Friend!</h1>
-                    <p>Enter your personal details and start your journey with us</p>
+                    <p>Enter your personal details and start your journey with us.</p>
                     <button id="signUp">Sign Up</button>
                 </div>
             </div>
@@ -201,8 +201,13 @@ $conn->close();
     <!-- js code -->
     <script>
         const container = document.getElementById('container');
+<<<<<<< HEAD
         const signInButton = document.getElementById('overlayCon');
         const signUpButton = document.getElementById('overlayCon');
+=======
+        const signInButton = document.getElementById('overlayBtn');
+        const signUpButton = document.getElementById('overlayBtn');
+>>>>>>> 4f93550c3b9c6932ca1a0cd90458901a0c1048e7
 
         signInButton.addEventListener('click', () => {
             container.classList.remove('right-panel-active');
