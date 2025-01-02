5720d509fee8de53ecefff1dd93b90c265701182
seyan <152641354+seyan5@users.noreply.github.com>
2025-01-03 00:36:33 +0800

    asdsad

--- admin/dashboard.php
+++ admin/dashboard.php
@@ -34,12 +34,10 @@ if ($result->num_rows > 0) {
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>La Sorpresa Admin</title>
-    <!-- ======= Styles ====== -->
     <link rel="stylesheet" href="../css/style.css">
 </head>
 
 <body>
-    <!-- =============== Navigation ================ -->
     <div class="container">
         <div class="navigation">
             <ul>
