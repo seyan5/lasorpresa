a3d3227981dd75cc61e7e6819e7676634476af2e
seyan <152641354+seyan5@users.noreply.github.com>
2025-01-03 00:58:40 +0800

    qweqweqasdsad

--- admin/delete-users.php
+++ admin/delete-users.php
@@ -2,7 +2,6 @@
 session_start();
 include '../config.php';
 
-// Check if user is logged in and is an admin
 if (!isset($_SESSION['user_id']) || $_SESSION['user_type'] != 'admin') {
     header('Location: ../login.php');
     exit();
