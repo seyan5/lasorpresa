f145cf22add487555662c74cafb416a8dc90be38
jwaynee <115685795+jwayne5@users.noreply.github.com>
2025-01-05 19:28:20 +0800

    tt

--- admin/settings.php
+++ admin/settings.php
@@ -1,5 +1,7 @@
 <?php
 session_start();
+include 'header.php';
+
 include 'inc/config.php';
 if (!isset($_SESSION['user_id']) || $_SESSION['user_type'] != 'admin') {
     header('Location: ../login.php');
