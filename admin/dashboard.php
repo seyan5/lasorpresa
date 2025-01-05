b5d81af39b06c981c260b8f6863e937625be90b1
jwaynee <115685795+jwayne5@users.noreply.github.com>
2025-01-05 15:37:00 +0800

    test

--- admin/dashboard.php
+++ admin/dashboard.php
@@ -105,24 +105,14 @@ if ($result->num_rows > 0) {
                     </a>
                 </li>
 
-                <li class="treeview <?php if( ($cur_page == 'size.php') || ($cur_page == 'size-add.php') || ($cur_page == 'size-edit.php') || ($cur_page == 'color.php') || ($cur_page == 'color-add.php') || ($cur_page == 'color-edit.php') || ($cur_page == 'country.php') || ($cur_page == 'country-add.php') || ($cur_page == 'country-edit.php') || ($cur_page == 'shipping-cost.php') || ($cur_page == 'shipping-cost-edit.php') || ($cur_page == 'top-category.php') || ($cur_page == 'top-category-add.php') || ($cur_page == 'top-category-edit.php') || ($cur_page == 'mid-category.php') || ($cur_page == 'mid-category-add.php') || ($cur_page == 'mid-category-edit.php') || ($cur_page == 'end-category.php') || ($cur_page == 'end-category-add.php') || ($cur_page == 'end-category-edit.php') ) {echo 'active';} ?>">
-                        <a href="#">
-                            <i class="fa fa-cogs"></i>
-                            <span>Shop Settings</span>
-                            <span class="pull-right-container">
-								<i class="fa fa-angle-left pull-right"></i>
-							</span>
-                        </a>
-                        <ul class="treeview-menu">
-                            <li><a href="size.php"><i class="fa fa-circle-o"></i> Size</a></li>
-                            <li><a href="color.php"><i class="fa fa-circle-o"></i> Color</a></li>
-                            <li><a href="country.php"><i class="fa fa-circle-o"></i> Country</a></li>
-                            <li><a href="shipping-cost.php"><i class="fa fa-circle-o"></i> Shipping Cost</a></li>
-                            <li><a href="top-category.php"><i class="fa fa-circle-o"></i> Top Level Category</a></li>
-                            <li><a href="mid-category.php"><i class="fa fa-circle-o"></i> Mid Level Category</a></li>
-                            <li><a href="end-category.php"><i class="fa fa-circle-o"></i> End Level Category</a></li>
-                        </ul>
-                    </li>
+                <li>
+                    <a href="settings/type.php">
+                        <span class="icon">
+                            <ion-icon name="settings-outline"></ion-icon>
+                        </span>
+                        <span class="title">Settings</span>
+                    </a>
+                </li>
 
                 <li>
                     <a href="../logout.php">
