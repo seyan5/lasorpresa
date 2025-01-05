2d7d65a6f3b7f050477ad66dc1309a3bda6ef973
jwaynee <115685795+jwayne5@users.noreply.github.com>
2025-01-05 16:55:59 +0800

    sql

--- lasorpresa.sql
+++ lasorpresa.sql
@@ -1,21 +1,15 @@
 -- phpMyAdmin SQL Dump
--- version 5.2.1
+-- version 5.1.3
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jan 05, 2025 at 06:35 AM
--- Server version: 10.4.32-MariaDB
--- PHP Version: 8.2.12
+-- Generation Time: Jan 05, 2025 at 09:54 AM
+-- Server version: 10.4.24-MariaDB
+-- PHP Version: 8.1.4
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
 
 --
 -- Database: `lasorpresa`
@@ -28,19 +22,17 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `color` (
-  `color_id` int(100) NOT NULL,
-  `color_name` varchar(100) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+  `color_id` int(11) NOT NULL,
+  `color_name` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `color`
 --
 
 INSERT INTO `color` (`color_id`, `color_name`) VALUES
-(1, 'Red'),
-(2, 'White'),
-(3, 'Purple'),
-(4, 'Blue');
+(0, 'red'),
+(0, 'blue');
 
 -- --------------------------------------------------------
 
@@ -50,8 +42,17 @@ INSERT INTO `color` (`color_id`, `color_name`) VALUES
 
 CREATE TABLE `end_category` (
   `ecat_id` int(11) NOT NULL,
-  `ecat_name` varchar(100) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+  `ecat_name` varchar(255) NOT NULL,
+  `mcat_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `end_category`
+--
+
+INSERT INTO `end_category` (`ecat_id`, `ecat_name`, `mcat_id`) VALUES
+(0, 'hay finally', 3),
+(0, 'red', 4);
 
 -- --------------------------------------------------------
 
@@ -61,8 +62,20 @@ CREATE TABLE `end_category` (
 
 CREATE TABLE `mid_category` (
   `mcat_id` int(11) NOT NULL,
-  `mcat_name` varchar(100) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+  `mcat_name` varchar(255) NOT NULL,
+  `tcat_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `mid_category`
+--
+
+INSERT INTO `mid_category` (`mcat_id`, `mcat_name`, `tcat_id`) VALUES
+(1, 'blue', 3),
+(2, 'test', 1),
+(3, 'blue11', 3),
+(4, 'Red11', 2),
+(5, 'Red1', 2);
 
 -- --------------------------------------------------------
 
@@ -85,28 +98,7 @@ CREATE TABLE `product` (
   `is_featured` int(1) NOT NULL,
   `is_active` int(1) NOT NULL,
   `ecat_id` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `product_color`
---
-
-CREATE TABLE `product_color` (
-  `id` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `product_photo`
---
-
-CREATE TABLE `product_photo` (
-  `pp_id` int(11) NOT NULL,
-  `photo` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -116,19 +108,38 @@ CREATE TABLE `product_photo` (
 
 CREATE TABLE `top_category` (
   `tcat_id` int(11) NOT NULL,
-  `tcat_name` varchar(100) NOT NULL,
+  `tcat_name` varchar(255) NOT NULL,
   `show_on_menu` int(1) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `top_category`
 --
 
 INSERT INTO `top_category` (`tcat_id`, `tcat_name`, `show_on_menu`) VALUES
-(1, 'Bouquets', 0),
-(2, 'Flowers', 0),
-(3, 'Bouquets', 1),
-(4, 'Flowers', 1);
+(1, '0', 1),
+(2, 'red', 1),
+(3, 'Blue', 0);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `type`
+--
+
+CREATE TABLE `type` (
+  `type_id` int(11) NOT NULL,
+  `type_name` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `type`
+--
+
+INSERT INTO `type` (`type_id`, `type_name`) VALUES
+(1, '222'),
+(2, 'tulip'),
+(3, 'test');
 
 -- --------------------------------------------------------
 
@@ -143,32 +154,21 @@ CREATE TABLE `users` (
   `username` varchar(100) NOT NULL,
   `contact` varchar(100) NOT NULL,
   `password` varchar(255) NOT NULL,
-  `user_type` varchar(1000) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+  `user_type` enum('admin','user') NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `users`
 --
 
 INSERT INTO `users` (`id`, `name`, `email`, `username`, `contact`, `password`, `user_type`) VALUES
-(1, 'seyan', 'asdasd@dasd.asd', 'seyan', '123123981232', '$2y$10$DH.E7L5uCFNLXTC7DDS1NOBxJQIbBcYmd.wbi3waj3LAzKjAX./aW', 'admin'),
-(2, 'qwe qwe', 'pogi@gmail.com', 'jay', '123', '$2y$10$MiWqvoinK9uuah0488SfN.HcVQy37EJG77Euo8UKS4w3Um4W0cUAu', 'user');
+(1, 'seyan asdas', 'asdasd@dasd.asd', 'seyan', '12312398123', '$2y$10$DH.E7L5uCFNLXTC7DDS1NOBxJQIbBcYmd.wbi3waj3LAzKjAX./aW', 'admin'),
+(2, 'johnwayne aquino', 'jw@gmail.com', 'jw', '09214991751', '$2y$10$AW9ZQOGuBRPBMN.8Z8piAOYlwxoN7ZYEom1OjMeGZvyou6pI5pW0W', 'admin'),
+(3, 'jw Johnwayne', 'jpdeogracias@gmail.com', 'kbkr', '123123', '$2y$10$2fzRKOpndF1FpYUPyTr2H.QijN19/vvvpbFrar/Vytd3qGD0u04ZW', 'user');
 
 --
 -- Indexes for dumped tables
 --
-
---
--- Indexes for table `color`
---
-ALTER TABLE `color`
-  ADD PRIMARY KEY (`color_id`);
-
---
--- Indexes for table `end_category`
---
-ALTER TABLE `end_category`
-  ADD PRIMARY KEY (`ecat_id`);
 
 --
 -- Indexes for table `mid_category`
@@ -177,28 +177,16 @@ ALTER TABLE `mid_category`
   ADD PRIMARY KEY (`mcat_id`);
 
 --
--- Indexes for table `product`
---
-ALTER TABLE `product`
-  ADD PRIMARY KEY (`p_id`);
-
---
--- Indexes for table `product_color`
---
-ALTER TABLE `product_color`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `product_photo`
---
-ALTER TABLE `product_photo`
-  ADD PRIMARY KEY (`pp_id`);
-
---
 -- Indexes for table `top_category`
 --
 ALTER TABLE `top_category`
   ADD PRIMARY KEY (`tcat_id`);
+
+--
+-- Indexes for table `type`
+--
+ALTER TABLE `type`
+  ADD PRIMARY KEY (`type_id`);
 
 --
 -- Indexes for table `users`
@@ -211,81 +199,28 @@ ALTER TABLE `users`
 --
 
 --
--- AUTO_INCREMENT for table `color`
---
-ALTER TABLE `color`
-  MODIFY `color_id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
-
---
--- AUTO_INCREMENT for table `end_category`
---
-ALTER TABLE `end_category`
-  MODIFY `ecat_id` int(11) NOT NULL AUTO_INCREMENT;
-
---
 -- AUTO_INCREMENT for table `mid_category`
 --
 ALTER TABLE `mid_category`
-  MODIFY `mcat_id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `product`
---
-ALTER TABLE `product`
-  MODIFY `p_id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `product_color`
---
-ALTER TABLE `product_color`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `product_photo`
---
-ALTER TABLE `product_photo`
-  MODIFY `pp_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `mcat_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT for table `top_category`
 --
 ALTER TABLE `top_category`
-  MODIFY `tcat_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `tcat_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `type`
+--
+ALTER TABLE `type`
+  MODIFY `type_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
-
---
--- Constraints for dumped tables
---
-
---
--- Constraints for table `end_category`
---
-ALTER TABLE `end_category`
-  ADD CONSTRAINT `end_category_ibfk_1` FOREIGN KEY (`ecat_id`) REFERENCES `mid_category` (`mcat_id`);
-
---
--- Constraints for table `mid_category`
---
-ALTER TABLE `mid_category`
-  ADD CONSTRAINT `mid_category_ibfk_1` FOREIGN KEY (`mcat_id`) REFERENCES `top_category` (`tcat_id`);
-
---
--- Constraints for table `product_color`
---
-ALTER TABLE `product_color`
-  ADD CONSTRAINT `product_color_ibfk_1` FOREIGN KEY (`id`) REFERENCES `color` (`color_id`),
-  ADD CONSTRAINT `product_color_ibfk_2` FOREIGN KEY (`id`) REFERENCES `product` (`p_id`);
-
---
--- Constraints for table `product_photo`
---
-ALTER TABLE `product_photo`
-  ADD CONSTRAINT `product_photo_ibfk_1` FOREIGN KEY (`pp_id`) REFERENCES `product` (`p_id`);
+  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
