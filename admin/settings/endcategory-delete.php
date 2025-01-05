b1202e97b2ea0afc627410b34d85155074a22ad7
jwaynee <115685795+jwayne5@users.noreply.github.com>
2025-01-05 22:44:56 +0800

    asd

--- admin/settings/endcategory-delete.php
+++ admin/settings/endcategory-delete.php
@@ -23,7 +23,10 @@ if(!isset($_REQUEST['id'])) {
 	// Getting all ecat ids
 	$statement = $pdo->prepare("SELECT * FROM product WHERE ecat_id=?");
 	$statement->execute(array($_REQUEST['id']));
-	$result = $statement->fetchAll(PDO::FETCH_ASSOC);							
+	$result = $statement->fetchAll(PDO::FETCH_ASSOC);		
+	
+	$p_ids = array(); 
+	
 	foreach ($result as $row) {
 		$p_ids[] = $row['p_id'];
 	}
