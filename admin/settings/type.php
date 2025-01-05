cb412f9e55beac0c062d1de55786dde817939725
jwaynee <115685795+jwayne5@users.noreply.github.com>
2025-01-05 15:22:44 +0800

    test

--- admin/settings/type.php
+++ admin/settings/type.php
@@ -59,7 +59,7 @@
                 </li>
 
                 <li>
-                    <a href="#">
+                    <a href="settings/type.php">
                         <span class="icon">
                             <ion-icon name="settings-outline"></ion-icon>
                         </span>
