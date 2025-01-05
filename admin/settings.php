10b44e0fc3eaf1764b91d0d9a43e6f305f67c70b
jwaynee <115685795+jwayne5@users.noreply.github.com>
2025-01-05 19:27:51 +0800

    tt

--- admin/settings.php
+++ admin/settings.php
@@ -1,5 +1,40 @@
-<?php require("header.php"); ?>
+<?php
+session_start();
+include 'inc/config.php';
+if (!isset($_SESSION['user_id']) || $_SESSION['user_type'] != 'admin') {
+    header('Location: ../login.php');
+    exit();
+}
 
+$user_id = $_SESSION['user_id'];
+$stmt = $conn->prepare("SELECT * FROM users WHERE id = ?");
+if ($stmt === false) {
+    die('prepare() failed: ' . htmlspecialchars($conn->error));
+}
+$stmt->bind_param("i", $user_id);
+$stmt->execute();
+$result = $stmt->get_result();
+
+if ($result->num_rows > 0) {
+    $row = $result->fetch_assoc();
+} else {
+    $row = null;
+}
+
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>La Sorpresa Admin</title>
+    <link rel="stylesheet" href="../css/style.css">
+    
+</head>
 <body>
     <div class="container">
         <div class="navigation">
