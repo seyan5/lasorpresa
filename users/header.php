bcf1de98c4edbe1e4c9cef1810b8cd3458f4e9cd
jwaynee <115685795+jwayne5@users.noreply.github.com>
2025-01-05 20:09:45 +0800

    ds

--- users/header.php
+++ users/header.php
@@ -2,9 +2,9 @@
 <?php
 ob_start();
 session_start();
-include("admin/inc/config.php");
-include("admin/inc/functions.php");
-include("admin/inc/CSRF_Protect.php");
+include("../admin/inc/config.php");
+include("../admin/inc/functions.php");
+include("../admin/inc/CSRF_Protect.php");
 $csrf = new CSRF_Protect();
 $error_message = '';
 $success_message = '';
