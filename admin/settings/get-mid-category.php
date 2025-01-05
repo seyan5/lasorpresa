0ecb12b49ec7e217aeb190163bfc22f6b6a0446b
jwaynee <115685795+jwayne5@users.noreply.github.com>
2025-01-05 17:03:43 +0800

    test

---
+++ admin/settings/get-mid-category.php
@@ -0,0 +1,17 @@
+<?php
+require_once('../header.php');
+
+if($_POST['id'])
+{
+	$id = $_POST['id'];
+	
+	$statement = $pdo->prepare("SELECT * FROM mid_category WHERE tcat_id=?");
+	$statement->execute(array($id));
+	$result = $statement->fetchAll(PDO::FETCH_ASSOC);
+	?><option value="">Select Mid Level Category</option><?php						
+	foreach ($result as $row) {
+		?>
+        <option value="<?php echo $row['mcat_id']; ?>"><?php echo $row['mcat_name']; ?></option>
+        <?php
+	}
+}
