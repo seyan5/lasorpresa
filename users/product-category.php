56beb2fa847885fb8bac55b979a82b60d0ea8312
seyan <152641354+seyan5@users.noreply.github.com>
2025-01-06 00:50:25 +0800

    asd

--- users/product-category.php
+++ users/product-category.php
@@ -1,7 +1,7 @@
 <?php require_once('header.php'); ?>
 
 <?php
-$statement = $pdo->prepare("SELECT * FROM settings WHERE id=1");
+$statement = $pdo->prepare("SELECT * FROM tbl_settings WHERE id=1");
 $statement->execute();
 $result = $statement->fetchAll(PDO::FETCH_ASSOC);                            
 foreach ($result as $row) {
