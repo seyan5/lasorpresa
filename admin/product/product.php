b96935b7a05c8f9011862df51e94eb1f38f3e2fb
jwaynee <115685795+jwayne5@users.noreply.github.com>
2025-01-06 12:11:16 +0800

    asd

--- admin/product/product.php
+++ admin/product/product.php
@@ -97,7 +97,7 @@
                 </li>
 
                 <li>
-                    <a href="../index.php">
+                    <a href="../logout.php">
                         <span class="icon">
                             <ion-icon name="log-out-outline"></ion-icon>
                         </span>
