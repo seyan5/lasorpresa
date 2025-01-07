8c69c131e706113c51ad1e2573ffbc423fca8178
jaycantemprate <166972268+jaycantemprate@users.noreply.github.com>
2025-01-07 19:15:07 +0800

    cart css

--- users/product-details.php
+++ users/product-details.php
@@ -38,13 +38,12 @@ if ($p_id) {
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600;700;800;900&display=swap"
         rel="stylesheet">
-
 </head>
 
 <body>
     <header>
         <a href="products.php" class="back">← Back to Products</a>
-        <a href="cart.php" class="back"> Cart</a>
+        <a href="shopcart.php" class="back"> Cart</a>
 
     </header>
     <main>
