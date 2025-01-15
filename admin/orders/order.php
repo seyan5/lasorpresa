f8e6bf00f5970f007e9d70aa5a99016aac6233ba
Wayne <aquinojwayne@gmail.com>
2025-01-15 21:41:56 +0800

    dd

--- admin/orders/order.php
+++ admin/orders/order.php
@@ -50,7 +50,7 @@ $totalPages = ceil($totalOrders / $perPage);
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Order Dashboard</title>
+  <title>Order Jay</title>
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
   <style>
