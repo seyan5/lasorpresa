1a50069c9701542c75b87368be220e37280c64ee
seyan <152641354+seyan5@users.noreply.github.com>
2025-01-15 23:00:15 +0800

    user change status

--- admin/edit-users.php
+++ admin/edit-users.php
@@ -1,29 +1,38 @@
 <?php
-    include("../config.php");
-    
-    if($_POST){
-        $result= $conn->query("SELECT * from customer");
-        $cust_name=$_POST['cust_name'];
-        $cust_email=$_POST["cust_email"];
-        $cust_phone = $_POST["cust_phone"];
-        $cust_address = $_POST["cust_address"];
-        $cust_city = $_POST["cust_city"];
-        $cust_zip = $_POST["cust_zip"];
-        $cust_status = $_POST['cust_status'];
-        $id=$_POST['cust_id00'];
+include("../config.php");
 
-        // Corrected SQL query
-        $sql = "UPDATE customer SET cust_name='$cust_name', cust_email='$cust_email', cust_phone='$cust_phone', cust_address='$cust_address', cust_city='$cust_city', cust_zip='$cust_zip' WHERE cust_id=$id";
-        $result = mysqli_query($conn, $sql);
-    
-        if ($result) {
-            header('Location: users.php');
-            exit();
-        } else {
-            die('Error updating record: ' . mysqli_error($conn));
-        }
+if ($_POST) {
+    // Retrieve form data
+    $cust_name = $_POST['cust_name'];
+    $cust_email = $_POST["cust_email"];
+    $cust_phone = $_POST["cust_phone"];
+    $cust_address = $_POST["cust_address"];
+    $cust_city = $_POST["cust_city"];
+    $cust_zip = $_POST["cust_zip"];
+    $cust_status = $_POST['cust_status'];
+    $id = $_POST['cust_id00'];
+
+    // SQL query to update user details and status
+    $sql = "UPDATE customer 
+            SET 
+                cust_name = '$cust_name', 
+                cust_email = '$cust_email', 
+                cust_phone = '$cust_phone', 
+                cust_address = '$cust_address', 
+                cust_city = '$cust_city', 
+                cust_zip = '$cust_zip', 
+                cust_status = '$cust_status'
+            WHERE cust_id = $id";
+
+    // Execute the query
+    $result = mysqli_query($conn, $sql);
+
+    // Check if the query was successful
+    if ($result) {
+        header('Location: users.php'); // Redirect to users page after successful update
+        exit();
+    } else {
+        die('Error updating record: ' . mysqli_error($conn)); // Output error if query fails
     }
+}
 ?>
-
-</body>
-</html>
